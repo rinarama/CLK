b5242f098da10bed45c72666195a3ae13e622f22
Kendy Ng & Rina Robinson & Sam Glickman <pair+kenobi82403.rinarama.sglickman17@devbootcamp.com>
2017-02-17 15:09:21 -0500

    tested request and friendship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 require 'faker'
 User.destroy_all
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Request.destroy_all
+Friendship.destroy_all
+Comment.destroy_all
+Post.destroy_all
+Like.destroy_all
+
+# creating users
 tom = User.create({
       email: "tom@myspace.com",
       password: "123456",
@@ -21,5 +21,16 @@ tom = User.create({
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
     })
-
 end
+byebug
+# Mary exists
+mary = User.last
+
+# Tom request her friendship
+req = tom.requests.create({requester_id: mary.id})
+
+# Mary accepts Tom's request
+acc_req = Request.find_by_user_id(tom.id).update(status: true)
+
+# Mary & Tom are part of a friendship
+friendship = mary.friendships.create({ follower_id: tom.id })
