e6a279d49e376783b3c17f02aba29d823a897208
Kendy <kenobi82403@gmail.com>
2017-02-18 06:51:56 +0000

    new post route works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to "/users/#{post_params[:user_id]}"
+    else
+      redirect_to "/users/#{post_params[:user_id]}"
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 end
