7d714d4d71754642a071145befb1e5597dbc0f27
Kendy Ng <email": null>
2017-02-19 17:11:09 -0500

    refactored like route to be flexible for other type of likeable types

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,23 +2,31 @@ class LikesController < ApplicationController
   before_action :require_login
 
   def create
-    @post = Post.find_by_id(like_params[:likeable_id])
-    @like = @post.likes.create(user_id: current_user.id)
+    if like_params[:likeable_type] == "post"
+      @type = Post.find_by_id(like_params[:likeable_id])
+    else
 
-    redirect_to "/users/#{@post.user_id}"
+    end
+    @like = @type.likes.create(user_id: current_user.id)
+
+    redirect_to "/users/#{@type.user_id}"
   end
 
   def destroy
-    @post = Post.find_by_id(like_params[:likeable_id])
-    @like = @post.likes.find_by_user_id(current_user.id).destroy
+    if like_params[:likeable_type] == "post"
+      @type = Post.find_by_id(like_params[:likeable_id])
+    else
+
+    end
+    @like = @type.likes.find_by_user_id(current_user.id).destroy
 
 
-    redirect_to "/users/#{@post.user_id}"
+    redirect_to "/users/#{@type.user_id}"
   end
 
   private
 
   def like_params
-    params.require(:like).permit(:likeable_id)
+    params.require(:like).permit(:likeable_type,:likeable_id)
   end
 end
