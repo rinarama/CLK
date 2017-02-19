e0ef3fd0bb0e228f6eafa6551242cce76d04fca3
Kendy Ng <email": null>
2017-02-19 17:18:31 -0500

    likes for comments work

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,23 +5,25 @@ class LikesController < ApplicationController
     if like_params[:likeable_type] == "post"
       @type = Post.find_by_id(like_params[:likeable_id])
     else
-
+      @type = Comment.find_by_id(like_params[:likeable_id])
+      @post = Post.find_by_id(@type.post_id)
     end
     @like = @type.likes.create(user_id: current_user.id)
 
-    redirect_to "/users/#{@type.user_id}"
+    redirect_to "/users/#{@post.user_id}"
   end
 
   def destroy
     if like_params[:likeable_type] == "post"
       @type = Post.find_by_id(like_params[:likeable_id])
     else
-
+      @type = Comment.find_by_id(like_params[:likeable_id])
+      @post = Post.find_by_id(@type.post_id)
     end
     @like = @type.likes.find_by_user_id(current_user.id).destroy
 
 
-    redirect_to "/users/#{@type.user_id}"
+    redirect_to "/users/#{@post.user_id}"
   end
 
   private
