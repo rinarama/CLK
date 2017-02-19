d3ee0d9d9d4e8bbcdd6415d6d2d37c05886a056c
Sam Glickman <email": null>
2017-02-19 13:16:45 -0500

    add create and destroy logic for like, like button now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @post = Post.new 
+    @post = Post.new
     @errors = []
     render layout: "landing"
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
-
-  validates :status, presence: true
 end
